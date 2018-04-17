5456012b76cd699040186e792348a0404c95bf76
Sala K405 <salak405@itmsalas.edu.co>
2018-04-16 21:30:11 -0500

    Funciones con Básica

---
+++ FUNCIONES.sql
@@ -0,0 +1,18 @@
+--RETORNA UN VALOR. SINO RETORNA ERROR
+ CREATE OR REPLACE FUNCTION findMin(x IN NUMBER, y IN NUMBER)
+  RETURN NUMBER IS 
+  Z NUMBER :=0;--DECLARACIÓN DE VARIALBES
+BEGIN
+    IF x>y THEN
+    Z:=y;
+  ELSE
+    Z:=X;
+  END IF;
+  RETURN Z;
+END;
+--primero se crea ésto
+--Luego ejecuta el begin:
+
+BEGIN
+   DBMS_OUTPUT.PUT_LINE('MIN 20-40:' || findMin(20,40));
+END;
